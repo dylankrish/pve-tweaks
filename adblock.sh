2dc2d550e5853b22658a507a6d29542dca61a8ed
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-06-21 09:39:23 -0400

    Backup proxmoxlib.js

--- adblock.sh
+++ adblock.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak
+
 sed -Ezi.bak "s/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 /usr/bin/systemctl restart pveproxy.service
