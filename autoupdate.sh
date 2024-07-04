429c4574363f057acd602da56483d3e8a3da26d0
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-07-04 03:02:48 -0400

    autoupdate

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# update & upgrade
 /usr/bin/apt update
 /usr/bin/apt full-upgrade -y
+
+# remove subscription warning
+cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak
+
+sed -Ezi.bak "s/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+/usr/bin/systemctl restart pveproxy.service
+
+# if system needs restart, reboot
+
+# else if services need restart, restart those services
